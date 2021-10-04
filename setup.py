c134f6e8af432e9472258623c440b454fa218dbc
Ivan Murabito <i.murabito@enerbrain.com>
2021-10-04 12:13:27 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name="curiousDolphin",
+      version="0.0.1",
+      author="isnob46@gmail.com",
+      url="https://github.com/CuriousDolphin/python_test_git_libraries.git",
+      packages=find_packages()
+      )
