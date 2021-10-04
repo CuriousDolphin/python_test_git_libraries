b7babdeb3158032d5adee803d9cf9a4c8c2309b9
Ivan Murabito <i.murabito@enerbrain.com>
2021-10-04 14:37:05 +0200

    wip

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="curiousDolphin",
-      version="0.0.1",
+      version="0.0.2",
       author="isnob46@gmail.com",
       url="https://github.com/CuriousDolphin/python_test_git_libraries.git",
       packages=find_packages()
